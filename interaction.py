5c6386cb16c1049589f85d76fb16a3f916813ee5
Biruk Zewdu <birukzewdugebreyohanes1219@gmail.com>
2025-05-31 14:16:45 +0300

    edited interaction

--- interaction.py
+++ interaction.py
@@ -1,8 +1,7 @@
 from datetime import datetime
-
 def days_since(last_date):
     if last_date == "Never":
         return float('inf')  # Means "never contacted"
-    
     last_contact = datetime.strptime(last_date, "%Y-%m-%d")
     return (datetime.now() - last_contact).days
+
